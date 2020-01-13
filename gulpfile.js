4eb9ed3c7911b1e5e3f03bea2eec5573f69a8170
Erik <erickopcha@gmail.com>
2020-01-13 14:43:40 +0200

    fixes css task

--- gulpfile.js
+++ gulpfile.js
@@ -120,7 +120,7 @@ gulp.task("server", function () {
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss}", gulp.series("css", "refresh"));
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/img/*.{png,jpg,svg}", gulp.series("images", "refresh"));
   gulp.watch("source/img/webp/*.webp", gulp.series("webp", "refresh"));
